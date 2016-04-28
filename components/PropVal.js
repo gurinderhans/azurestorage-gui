cde58a16918c0434ded605912246f87b828bbfe7
Gurinder Hans <hello@gurinderhans.me>
2016-04-27 21:49:58 -0700

    fix <PropVal> not updating issue

--- components/PropVal.js
+++ components/PropVal.js
@@ -3,28 +3,47 @@ import React from 'react';
 const PropVal = React.createClass({
 	getInitialState() {
 		return {
-			propKey: this.props.propKey || '',
-			propVal: this.props.propVal || ''
+			propKey: '',
+			propVal: ''
+		}
+	},
+
+	componentWillMount() {
+		this.setState({
+			propKey: this.props.propKey,
+			propVal: this.props.propVal
+		});
+	},
+
+	componentWillReceiveProps(newProps) {
+		if (this.state.propKey != newProps.propKey || this.state.propVal != newProps.propVal) {
+			this.setState({
+				propKey: newProps.propKey,
+				propVal: newProps.propVal
+			});
 		}
 	},
 
 	handlePropChange(event) {
 		this.setState({
 			propKey: event.target.value,
-		})
+		});
 	},
 
 	handleValueChange(event) {
 		this.setState({
 			propVal: event.target.value,
-		})
+		});
 	},
 
 	render() {
+		const propKey = this.state.propKey || '';
+		const propVal = this.state.propVal || '';
+
 		return (
 			<div>
-				<input type='text' placeholder='Prop' value={this.state.propKey} onChange={this.handlePropChange} />
-				<input type='text' placeholder='Value' value={this.state.propVal} onChange={this.handleValueChange} />
+				<input type='text' placeholder='Prop' value={propKey} onChange={this.handlePropChange} />
+				<input type='text' placeholder='Value' value={propVal} onChange={this.handleValueChange} />
 			</div>
 		);
 	}
