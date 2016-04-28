3f9db27c35b8e80fbddde6016d59b3dfe3e7bc92
Gurinder Hans <hello@gurinderhans.me>
2016-04-27 19:04:58 -0700

    make <PropVal> controlled, since it needs to be updated

--- components/PropVal.js
+++ components/PropVal.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 const PropVal = React.createClass({
+	getInitialState() {
+		return {
+			propKey: this.props.propKey || '',
+			propVal: this.props.propVal || ''
+		}
+	},
+
+	handlePropChange(event) {
+		this.setState({
+			propKey: event.target.value,
+		})
+	},
+
+	handleValueChange(event) {
+		this.setState({
+			propVal: event.target.value,
+		})
+	},
 
 	render() {
 		return (
 			<div>
-				<input type='text' placeholder='Prop' defaultValue={this.props.propKey} />
-				<input type='text' placeholder='Value' defaultValue={this.props.propVal} />
+				<input type='text' placeholder='Prop' value={this.state.propKey} onChange={this.handlePropChange} />
+				<input type='text' placeholder='Value' value={this.state.propVal} onChange={this.handleValueChange} />
 			</div>
 		);
 	}
