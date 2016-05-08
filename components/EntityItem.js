9440bb41cabee46b09e8a9a8bb25a57a178d5299
Gurinder Hans <hello@gurinderhans.me>
2016-05-07 21:39:25 -0700

    dynamic value field added for <EntityItem> value

--- components/EntityItem.js
+++ components/EntityItem.js
@@ -43,10 +43,28 @@ const EntityItem = React.createClass({
 	},
 
 	render() {
+
+		let entityItemValueField;
+		if (this.props.item.type === 'boolean') {
+			entityItemValueField = (
+				<select value={this.props.item.val} onChange={this.fieldValOnChange}>
+					<option value='true'>True</option>
+					<option value='false'>False</option>
+				</select>
+			);
+		} else if (this.props.item.type === 'datetime') {
+			let date = this.props.item.val ? new Date(this.props.item.val) : new Date();
+			entityItemValueField = (<input key={this.props.item.type} type='datetime-local' value={date.toISOString().slice(0,-1)} onChange={this.fieldValOnChange} />);
+		} else if (this.props.item.type === 'number') {
+			entityItemValueField = (<input key={this.props.item.type} type='number' placeholder='Value' value={this.props.item.val} onChange={this.fieldValOnChange} />);
+		} else {
+			entityItemValueField = (<input key={this.props.item.type} type='text' placeholder='Value' value={this.props.item.val} onChange={this.fieldValOnChange} />);
+		}
+
 		return (
 			<div>
 				<input type='text' placeholder='Prop' value={this.props.item.key} onChange={this.fieldKeyOnChange} />
-				<input type='text' placeholder='Value' value={this.props.item.val} onChange={this.fieldValOnChange} />
+				{entityItemValueField}
 				<select value={this.props.item.type} onChange={this.fieldTypeOnChange}>
 					<option value='string'>String</option>
 					<option value='number'>Number</option>
