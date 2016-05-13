920cfc352886c0f5c15583be05a064e0d8af40ef
Gurinder Hans <hello@gurinderhans.me>
2016-05-12 23:59:41 -0700

    add sample react component tests, changed .travis.yml to accomodate

---
+++ uitest/burrito-test.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var expect = require('chai').expect;
+var enzyme = require('enzyme');
+var Burrito = require('../Burrito');
+
+describe("<Burrito />", function() {
+  'use strict';
+
+  describe('Text: ', function () {
+    it("renders no text with no props", function() {
+      var wrapper = enzyme.shallow(<Burrito />);
+      expect(wrapper.text()).to.equal('');
+    });
+
+    it("renders no text with no props", function() {
+      var wrapper = enzyme.shallow(<Burrito ingredients={['chicken', 'beans']} />);
+      expect(wrapper.text()).to.equal('chicken, beans');
+    });
+  });
+
+  describe('Name: ', function() {
+    it("is named 'burrito' with no props", function() {
+      var wrapper = enzyme.shallow(<Burrito />);
+      expect(wrapper.hasClass('burrito')).to.equal(true);
+    });
+
+    it("is named 'the-james-mason' with 'the-james-mason' passed as name", function() {
+      var wrapper = enzyme.shallow(<Burrito name='the-james-mason' />);
+      expect(wrapper.hasClass('the-james-mason')).to.equal(true);
+    });
+
+  });
+});
