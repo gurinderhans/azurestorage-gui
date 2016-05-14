b181df9a5d1d17927e51a05ac716d8d6d467b1ed
Gurinder Hans <hello@gurinderhans.me>
2016-05-13 22:09:04 -0700

    disable UI Tests for now until application has proper UI
    on side note: add <TablesList> for loading and deleting tables

--- test/components/app-test.js
+++ test/components/app-test.js
@@ -13,15 +13,19 @@ import TablesList from '../../components/TablesList'
 	global.window = doc.defaultView;
 }
 
+// about 2secs for each request is fine for the roundtrip
+const SERVER_REQ_TIMEOUT = 2000;
+
 /// MARK: - Tests
+
 describe('<TablesList />', function() {
 	it('displays tables', () => {
 		const tablesList = shallow(<TablesList tableClickHandle={() => {}} />);
 		const tablesToAdd = ['table1', 'table2'];
 		tablesList.setState({tables: tablesToAdd});
 
-		expect(tablesList.find('li')).to.have.length(tablesToAdd.length);
-		expect(tablesList.find('li > span').first().text()).to.equal(tablesToAdd[0]);
+		expect(tablesList.find('ul > li')).to.have.length(tablesToAdd.length);
+		expect(tablesList.find('ul > li > span').first().text()).to.equal(tablesToAdd[0]);
 	});
 
 	it('can type in new table name', () => {
@@ -38,5 +42,43 @@ describe('<TablesList />', function() {
 		// test `null`
 		tblNameInput.simulate('change', {target: {value: null}});
 		expect(tablesList.state().newTableName).to.equal(null);
+	});
+
+	it('can create table', done => {
+
+		const tablesList = shallow(<TablesList tableClickHandle={() => {}} />);
+		const tblNameInput = tablesList.find('input[type="text"]');
+
+		expect(tablesList.find('ul > li')).to.have.length(0);
+
+		tblNameInput.simulate('change', {target: {value: 'newTable'}});
+
+		const addTableBtn = tablesList.find('div > button');
+		addTableBtn.simulate('click');
+
+		setTimeout(() => {
+			tablesList.update();
+			expect(tablesList.find('ul > li')).to.have.length(1);
+			done();
+		}, SERVER_REQ_TIMEOUT);
+	});
+
+	it('loads table list and deletes first one', done => {
+		const tablesList = mount(<TablesList tableClickHandle={() => {}} />);
+
+		setTimeout(() => {
+			tablesList.update();
+			expect(tablesList.find('ul > li')).to.have.length(1);
+
+			// delete table
+			const tblToDelete = tablesList.find('ul > li').first();
+			tblToDelete.find('button').simulate('click');
+
+			setTimeout(() => {
+				tablesList.update();
+				expect(tablesList.find('ul > li')).to.have.length(0);
+				done();
+			}, SERVER_REQ_TIMEOUT);
+		}, SERVER_REQ_TIMEOUT);
 	});
 });
