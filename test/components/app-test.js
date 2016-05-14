5e9a6dcb33d805bd67ea5368d8eff20d65541e7d
Gurinder Hans <hello@gurinderhans.me>
2016-05-13 20:28:34 -0700

    anotehr <TablesList /> test added, tests new table name inpit change

--- test/components/app-test.js
+++ test/components/app-test.js
@@ -17,9 +17,26 @@ import TablesList from '../../components/TablesList'
 describe('<TablesList />', function() {
 	it('displays tables', () => {
 		const tablesList = shallow(<TablesList tableClickHandle={() => {}} />);
-		tablesList.setState({tables: ['table1', 'table2']});
+		const tablesToAdd = ['table1', 'table2'];
+		tablesList.setState({tables: tablesToAdd});
 
-		expect(tablesList.find('li')).to.have.length(2);
-		expect(tablesList.find('li > span').first().text()).to.equal('table1');
+		expect(tablesList.find('li')).to.have.length(tablesToAdd.length);
+		expect(tablesList.find('li > span').first().text()).to.equal(tablesToAdd[0]);
+	});
+
+	it('can type in new table name', () => {
+
+		const tablesList = shallow(<TablesList tableClickHandle={() => {}} />);
+		const tblNameInput = tablesList.find('input[type="text"]');
+
+		expect(tblNameInput).to.exist;
+		
+		const inputVal = 'new value';
+		tblNameInput.simulate('change', {target: {value: inputVal}});
+		expect(tablesList.state().newTableName).to.equal(inputVal);
+
+		// test `null`
+		tblNameInput.simulate('change', {target: {value: null}});
+		expect(tablesList.state().newTableName).to.equal(null);
 	});
 });
